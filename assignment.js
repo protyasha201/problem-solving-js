e95cb55ac66cfc2f2abade9924842cd4e9ecc705
protyasha201 <protyasha0999@gmail.com>
2021-01-22 00:06:27 +0600

    second function completed

--- assignment.js
+++ assignment.js
@@ -1,13 +1,34 @@
 // https://github.com/protyasha201/problem-solving-js
 
-function kilometerToMeter(kilometer){
-    if(kilometer >= 0){
+
+
+//kilometer to meter converter.
+
+function kilometerToMeter(kilometer) {
+    if (kilometer >= 0) {
         meter = kilometer * 1000;
         return meter;
     }
-    else{
+    else {
         return "Distance can't be negative!";
     }
 }
-var kilo = kilometerToMeter(-2);
-console.log(kilo);
+
+
+//Budget calculator.
+
+function budgetCalculator(wactchQuantity, phoneQuantity, laptopQuantity) {
+    if (wactchQuantity, phoneQuantity, laptopQuantity < 0) {
+        return "Quantity can't be negative!";
+    }
+    if(wactchQuantity && phoneQuantity && laptopQuantity == 0){
+        return "You must buy at least one product."
+    }
+    else{
+        var totalWatchCost = wactchQuantity * 50;
+        var totalPhoneCost = phoneQuantity * 100;
+        var totalLaptopCost = laptopQuantity * 500;
+        var totalCost = totalWatchCost + totalPhoneCost + totalLaptopCost;
+        return totalCost;
+    }
+}
