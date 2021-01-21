b6523dcebc24461f9abb39d660cc1d5ce433f9c1
protyasha201 <protyasha0999@gmail.com>
2021-01-21 22:45:39 +0600

    completed first function

--- assignment.js
+++ assignment.js
@@ -1,1 +1,13 @@
-//hello world
+// https://github.com/protyasha201/problem-solving-js
+
+function kilometerToMeter(kilometer){
+    if(kilometer >= 0){
+        meter = kilometer * 1000;
+        return meter;
+    }
+    else{
+        return "Distance can't be negative!";
+    }
+}
+var kilo = kilometerToMeter(-2);
+console.log(kilo);
