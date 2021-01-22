3ad1841770c710b345c83d6b68b42bf0cac47045
protyasha201 <protyasha0999@gmail.com>
2021-01-22 13:16:25 +0600

    updated fourth function

--- assignment.js
+++ assignment.js
@@ -10,21 +10,23 @@ function kilometerToMeter(kilometer) {
         return meter;
     }
     else {
-        return "Distance can't be negative!";
+        return "Distance can't be negative or a string!";
     }
 }
+
+
 
 
 //Budget calculator.
 
 function budgetCalculator(wactchQuantity, phoneQuantity, laptopQuantity) {
-    if (wactchQuantity, phoneQuantity, laptopQuantity < 0) {
+    if (wactchQuantity < 0 || phoneQuantity < 0 || laptopQuantity < 0) {
         return "Quantity can't be negative!";
     }
-    if(wactchQuantity && phoneQuantity && laptopQuantity == 0){
+    else if (wactchQuantity == 0 && phoneQuantity == 0 && laptopQuantity == 0) {
         return "You must buy at least one product."
     }
-    else{
+    else {
         var totalWatchCost = wactchQuantity * 50;
         var totalPhoneCost = phoneQuantity * 100;
         var totalLaptopCost = laptopQuantity * 500;
@@ -34,27 +36,43 @@ function budgetCalculator(wactchQuantity, phoneQuantity, laptopQuantity) {
 }
 
 
-//Hotel charge calculator.
 
-function hotelCost(stayingDays){
+
+//Hotel cost calculator.
+
+function hotelCost(stayingDays) {
     var totalHotelCharge = 0;
     var remainingDays = 0;
-    if(stayingDays < 0){
+    if (stayingDays < 0) {
         return "Number of day's must be positive."
     }
-    else if(stayingDays <= 10){
+    else if (stayingDays <= 10) {
         totalHotelCharge = 100 * stayingDays;
     }
-    else if(stayingDays <= 20){
+    else if (stayingDays <= 20) {
         remainingDays = stayingDays - 10;
         totalHotelCharge = (80 * remainingDays) + 1000;
     }
-    else{
+    else {
         remainingDays = stayingDays - 20;
         totalHotelCharge = (50 * remainingDays) + 1800;
     }
     return totalHotelCharge;
 }
 
-var days = hotelCost(2);
-console.log(days);
+
+
+//Find the biggest name.
+
+function megaFriend(names) {
+    var biggestName = names[0];
+    for (var i = 0; i < names.length; i++) {
+        if (typeof (names[i]) == "number") {
+            return "can't access numbers!";
+        }
+        else if (names[i].length > biggestName.length) {
+            biggestName = names[i];
+        }
+    }
+    return biggestName;
+}
